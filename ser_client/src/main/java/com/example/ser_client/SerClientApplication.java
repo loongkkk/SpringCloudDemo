db0c51696e6f4582bf4f25dff8d3a51193d17e8a
loong <van.vvgo@gmail.com>
2021-08-17 20:03:13 +0800

    创建文件目录

---
+++ ser_client/src/main/java/com/example/ser_client/SerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.ser_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerClientApplication.class, args);
+    }
+
+}
