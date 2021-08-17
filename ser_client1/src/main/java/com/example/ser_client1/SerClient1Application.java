161c7658726edb4287031eb5fd6f7ce8343f544b
loong <van.vvgo@gmail.com>
2021-08-17 20:52:01 +0800

    新增模块

---
+++ ser_client1/src/main/java/com/example/ser_client1/SerClient1Application.java
@@ -0,0 +1,13 @@
+package com.example.ser_client1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerClient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerClient1Application.class, args);
+    }
+
+}
---
+++ ser_client1/src/test/java/com/example/ser_client1/SerClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ser_client1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerClient1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
