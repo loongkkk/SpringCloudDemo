107c59ddfdfe80ea50d7c6771d6baac381d544dc
loong <van.vvgo@gmail.com>
2021-08-17 22:08:29 +0800

    新增网关模块

---
+++ ser_client2/src/main/java/com/example/ser_client2/SerClient2Application.java
@@ -0,0 +1,13 @@
+package com.example.ser_client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerClient2Application.class, args);
+    }
+
+}
---
+++ ser_client2/src/test/java/com/example/ser_client2/SerClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ser_client2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
