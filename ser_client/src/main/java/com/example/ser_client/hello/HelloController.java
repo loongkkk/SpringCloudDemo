b94dc94dae810610874b7b38e777a580f527534f
loong <van.vvgo@gmail.com>
2021-08-18 20:44:25 +0800

    新增端口输出

--- ser_client/src/main/java/com/example/ser_client/hello/HelloController.java
+++ ser_client/src/main/java/com/example/ser_client/hello/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.ser_client.hello;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Configuration
 @Controller
 public class HelloController {
+
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello(){
-        return "Hello";
+        return "server.port:" + port + " Hello";
     }
 }
--- ser_client1/src/main/java/com/example/ser_client1/hello/HelloController.java
+++ ser_client1/src/main/java/com/example/ser_client1/hello/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.ser_client1.hello;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Configuration
 @Controller
 public class HelloController {
+
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello(){
-        return "Hello world";
+        return "server.port:" + port + " Hello world";
     }
 }
