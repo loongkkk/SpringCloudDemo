f5b06102ce6c7c64c8088e20fa20d20f34e2d7dc
loong <van.vvgo@gmail.com>
2021-08-18 23:11:59 +0800

    优化显示

--- ser_client/src/main/java/com/example/ser_client/hello/HelloController.java
+++ ser_client/src/main/java/com/example/ser_client/hello/HelloController.java
@@ -6,6 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
 @Configuration
 @Controller
 public class HelloController {
@@ -15,7 +22,30 @@ public class HelloController {
 
     @GetMapping("/hello")
     @ResponseBody
-    public String hello(){
-        return "server.port:" + port + " Hello";
+    public String hello(HttpServletRequest request){
+        return  "server.ip:" + getIpAddr(request) + "\n server.port:" + port + " \nHello";
+    }
+
+    public static String getIpAddr(HttpServletRequest request){
+        try{
+            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (allNetInterfaces.hasMoreElements()){
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements()){
+                    InetAddress ip = (InetAddress) addresses.nextElement();
+                    if (ip != null
+                            && ip instanceof Inet4Address
+                            && !ip.isLoopbackAddress() //loopback地址即本机地址，IPv4的loopback范围是127.0.0.0 ~ 127.255.255.255
+                            && ip.getHostAddress().indexOf(":")==-1){
+                        //System.out.println("本机的IP = " + ip.getHostAddress());
+                        return ip.getHostAddress();
+                    }
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
